130413709f9809561128038482090f0893684dce
NnatuanyaFrankOguguo <nnatuanyafrank@gmail.com>
2025-03-11 20:42:02 +0100

    added link to backend for socketio

--- fun-number-ui/src/components/socketMessages.tsx
+++ fun-number-ui/src/components/socketMessages.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:5000");
+const socket = io("https://fun-fact-apis.onrender.com");
 
 const SocketMessages: React.FC = () => {
     const [messages, setMessages] = useState<any[]>([]);
